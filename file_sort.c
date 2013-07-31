d36dd5e27506692ffee9ad3d2cef149c18791830
nachivpn <nachivpn@gmail.com>
2013-07-31 18:33:57 +0530

    Completed with quick sort

--- file_sort.c
+++ file_sort.c
@@ -1,4 +1,11 @@
-/*Parse a file with numbers separated by commas and sort them*/
+/*
+	Problem statement : Parse a file with numbers separated by commas and sort them
+	Design :
+		- Parses the input file using FSM simulation
+		- Stores the numbers in dynamic floating point array
+		- Uses Quicksort to sort the floating point array
+
+*/
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,6 +14,8 @@
 #define I 	1
 #define II	2
 #define III	3
+#define M	10
+#define P	11
 
 /*Input Types*/
 #define NUM	4
@@ -14,9 +23,57 @@
 #define	DOT	6	
 #define CR	7
 #define ERR	8
+#define NEG	9
 
 float* arr;
 int used=0;
+
+void display(int i,int max)
+{
+	for(i;i<max;i++)	
+		(i==0)?printf("%f",arr[i]):printf(",%f",arr[i]);
+	printf("\n");
+}
+
+void rev_display(int i,int end)
+{	
+	int j;
+	for(j=i-1;j>=end;j--)	
+		(j==i-1)?printf("%f",arr[j]):printf(",%f",arr[j]);
+	printf("\n");
+}
+
+void quicksort(float *array,int p, int r)
+{
+	if(p<r)
+	{
+		int q = partition(array,p,r);
+		quicksort(array,p,q-1);
+		quicksort(array,q+1,r);
+	}
+	//display();
+}
+
+int partition(float *array,int p,int r)
+{
+	float x=array[r];
+	int j,i=p-1;
+	for(j=p;j<=r-1;j++)
+	{
+		if(array[j]<=x)
+		{
+			i++;
+			float temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+	float temp_1 = array[i+1];
+	array[i+1] = array[r];
+	array[r] = temp_1;
+	return i+1;
+}
+
 
 void error()
 {
@@ -31,12 +88,7 @@ void insert(float value)
 	arr=(float*) realloc(arr,sizeof(float));
 }
 
-void display()
-{
-	int i;
-	for(i=0;i<used;i++)	
-		(i==0)?printf("%f",arr[i]):printf(",%f",arr[i]);
-}
+
 
 int main()
 {
@@ -45,9 +97,9 @@ int main()
 	fp=fopen("test_file_sort","r");
 	if(fp == NULL)
 		exit(0);
-	int input_type,dec_pos;
+	int input_type,sign=P;
 	int prev_val,VAL;
-	float prev_dval,dec_denom;
+	float prev_dval,dec_denom=1;
 	int curr_state = N;	
 	char ch = getc(fp);
 	while( !feof(fp) )
@@ -64,6 +116,8 @@ int main()
 			input_type = DOT;
 		else if(ascii_val==10)
 			input_type = CR;
+		else if(ascii_val==45)
+			input_type = NEG;
 		else
 			input_type = ERR;
 		switch(input_type)
@@ -82,8 +136,7 @@ int main()
 				}
 				else if(curr_state == II)
 				{
-					dec_pos++;
-					dec_denom=dec_pos*10;					
+					dec_denom*=10;					
 					curr_state=II;
 					prev_dval=prev_dval+(VAL/dec_denom);
 				}
@@ -96,81 +149,61 @@ int main()
 			}
 			case DOT:
 			{
-				if(curr_state == N)
+				if(curr_state == I)
 				{
-					error();
-					break;
-				}
-				else if(curr_state == I)
-				{
-					dec_pos=0;					
 					curr_state=II;
 					prev_dval=(float)prev_val;
 				}
-				else if(curr_state == II)
-				{
+				else 
 					error();
-					break;
-				}
-				else if(curr_state == III)
-				{
-					error();
-					break;
-				}
 					
-				break;
 				break;
 			}
 			case COM :
 			{
-				if(curr_state == N)
-				{	
-					error();
-					break;
-				}
-				else if(curr_state == I)
+				if(curr_state == I)
 				{
-					insert((float)prev_val);
+					(sign==M)?insert((float)prev_val*-1):insert((float)prev_val);
 					curr_state=III;
 					prev_val=0;
+					sign=P;
 				}
 				else if(curr_state == II)
 				{
-					insert(prev_dval);
+					(sign==M)?insert(prev_dval*-1):insert(prev_dval);
 					curr_state=III;
 					prev_dval=0;
+					dec_denom=1;
+					sign=P;
 				}	
-				else if(curr_state == III)
-				{
+				else 
 					error();
-					break;
+				break;
+			}
+			case NEG:
+			{
+				if(curr_state == N || curr_state == III)
+				{
+					sign = M;
 				}
+				else 
+					error();
 				break;
 			}
 			case CR :
 			{
-				if(curr_state == N)
-				{	
-					error();
-					break;
-				}
-				else if(curr_state == I)
+				if(curr_state == I)
 				{
-					//printf("%d\nComplete",prev_val);
 					insert((float)prev_val);
 					break;
 				}
 				else if(curr_state == II)
 				{
-					//printf("%f\nComplete",prev_dval);
 					insert(prev_dval);
 					break;
 				}	
-				else if(curr_state == III)
-				{
+				else 
 					error();
-					break;
-				}
 				break;
 			}
 			case ERR:
@@ -179,8 +212,13 @@ int main()
 		ch=getc(fp);
 	}	
 	fclose(fp);
-	display();	
+	printf("Originial array :\n");
+	display(0,used);
+	printf("Array sorted in ascending order :\n");
+	quicksort(arr,0,used-1);
+	display(0,used);
+	printf("Array sorted in descending order :\n");
+	rev_display(used,0);	
 	free(arr);
 	return 1;
 }	
-
